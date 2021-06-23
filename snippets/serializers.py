e44a550d84f2a57126a9589b8108a36a9bfad1e0
Mask <planosmask@gmail.com>
2021-06-23 20:19:39 -0300

    API com autenticão

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,8 +1,20 @@
 from django.db.models import fields
 from rest_framework import serializers
 from snippets.models import Snippet
+from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'style']
+       
+        fields = [ 'owner', 'id', 'title', 'code', 'linenos', 'style']
+        
+        
+
+class UserSerializer(serializers.ModelSerializer):
+    Snippet = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
