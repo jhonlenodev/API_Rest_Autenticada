29e469f5734203e8de4caad91dddee0fcce320e9
Mask <planosmask@gmail.com>
2021-06-23 20:56:47 -0300

    Adiconado o endpoint

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,10 +5,13 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('snippets/<int:pk>/higlight/', views.SnippetHighlight.as_view()),
 
     path('api-auth/', include('rest_framework.urls')),
 
     path('user/', views.UserList.as_view()),
-    path('user/<int:pk>', views.UserDetail.as_view())
+    path('user/<int:pk>', views.UserDetail.as_view()),
+    path('', views.api_root),
+    
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
